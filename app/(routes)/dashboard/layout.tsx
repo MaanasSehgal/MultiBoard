141364f72363866f3e49a1cc942d54d126ea6593
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-06-17 01:24:10 +0530

    configured toast sonner for teams/create

---
+++ app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import {api} from "@/convex/_generated/api";
+import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
+import {useConvex} from "convex/react";
+import {useRouter} from "next/navigation";
+import React, {useEffect} from "react";
+
+const DashboardLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    const router = useRouter();
+    const convex = useConvex();
+    const {user}: any = useKindeBrowserClient();
+    useEffect(() => {
+        user && checkTeam();
+    }, [user]);
+
+    const checkTeam = async () => {
+        const result = await convex.query(api.teams.getTeam, {email: user?.email});
+        if (!result?.length) {
+            router.push("teams/create");
+        }
+    };
+    return <div>{children}</div>;
+};
+
+export default DashboardLayout;
